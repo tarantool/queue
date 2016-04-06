063613ea3924449e542936d1ee1194e29108aaed
bigbes <bigbes@gmail.com>
2016-04-06 21:17:22 +0300

    Replace download.tarantool.org with packagecloud.io

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
-curl http://download.tarantool.org/tarantool/1.6/gpgkey | sudo apt-key add -
+curl https://packagecloud.io/tarantool/1_6/gpgkey | sudo apt-key add -
 release=`lsb_release -c -s`
 
+sudo apt-get install -y apt-transport-https
+
 sudo tee /etc/apt/sources.list.d/tarantool_1_6.list <<- EOF
-deb http://download.tarantool.org/tarantool/1.6/ubuntu/ $release main
-deb-src http://download.tarantool.org/tarantool/1.6/ubuntu/ $release main
+deb https://packagecloud.io/tarantool/1_6/ubuntu/ $release main
+deb-src https://packagecloud.io/tarantool/1_6/ubuntu/ $release main
 EOF
 
 sudo apt-get update > /dev/null
