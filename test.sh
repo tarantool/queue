28d87d539305d7859f29eb0e1a8c3b16fad52a24
bigbes <bigbes@gmail.com>
2016-04-06 14:54:16 +0300

    replace debian to ubuntu in test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@ curl http://download.tarantool.org/tarantool/1.6/gpgkey | sudo apt-key add -
 release=`lsb_release -c -s`
 
 sudo tee /etc/apt/sources.list.d/tarantool_1_6.list <<- EOF
-deb http://download.tarantool.org/tarantool/1.6/debian/ $release main
-deb-src http://download.tarantool.org/tarantool/1.6/debian/ $release main
+deb http://download.tarantool.org/tarantool/1.6/ubuntu/ $release main
+deb-src http://download.tarantool.org/tarantool/1.6/ubuntu/ $release main
 EOF
 
 sudo apt-get update > /dev/null
