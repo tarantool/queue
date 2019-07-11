91a2777a74f0fba727eb418c80cae36c7658c1a1
Andrei Nepsha <nepsha.a.a@ya.ru>
2019-07-11 12:46:10 +0300

    Refactor limfifottl new method

--- queue/abstract/driver/limfifottl.lua
+++ queue/abstract/driver/limfifottl.lua
@@ -8,43 +8,35 @@ tube.create_space = fifottl.create_space
 
 -- start tube on space
 function tube.new(space, on_task_change, opts)
-    return setmetatable(
-        {
-            capacity = opts.capacity or 0,
-            parent = fifottl.new(space, on_task_change, opts)
-        },
-        {
-            __index = function(self, key)
-                if methods[key] ~= nil then
-                    return methods[key]
-                else
-                    return self.parent[key]
+    local state = {
+        capacity = opts.capacity or 0,
+        parent = fifottl.new(space, on_task_change, opts)
+    }
+
+    -- put task in space
+    local put = function (self, data, opts)
+        local timeout = opts.timeout or 0
+        timeout = timeout * 1000000
+
+        while true do
+            local tube_size = self.space:count()
+            if tube_size < state.capacity or state.capacity == 0 then
+                return state.parent.put(self, data, opts)
+            else
+                if timeout == 0 then
+                    return nil
                 end
+
+                local started = fiber.time64()
+                fiber.yield()
+                local elapsed = fiber.time64() - started
+
+                timeout = timeout > elapsed and timeout - elapsed or 0
             end
-        })
-end
-
--- put task in space
-function methods.put(self, data, opts)
-    local timeout = opts.timeout or 0
-    timeout = timeout * 1000000
-
-    while true do
-        local tube_size = self.space:count()
-        if tube_size < self.capacity or self.capacity == 0 then
-            return self.parent.put(self, data, opts)
-        else
-            if timeout == 0 then
-                return nil
-            end
-
-            local started = fiber.time64()
-            fiber.yield()
-            local elapsed = fiber.time64() - started
-
-            timeout = timeout > elapsed and timeout - elapsed or 0
         end
     end
+
+    return setmetatable({put = put}, {__index = state.parent})
 end
 
 return tube
--- t/100-limfifottl.t
+++ t/100-limfifottl.t
@@ -3,7 +3,7 @@ local yaml  = require('yaml')
 local fiber = require('fiber')
 
 local test = require('tap').test()
-test:plan(7)
+test:plan(8)
 
 local queue = require('queue')
 local state = require('queue.abstract.state')
@@ -16,10 +16,11 @@ tnt.cfg{}
 test:ok(rawget(box, 'space'), 'box started')
 test:ok(queue, 'queue is loaded')
 
-local tube = queue.create_tube('test', 'limfifottl', { engine = engine, capacity = 3 })
+local tube = queue.create_tube('lim3_tube', 'limfifottl', { engine = engine, capacity = 3 })
+local unlim_tube = queue.create_tube('unlim_tube', 'limfifottl', { engine = engine })
 
 test:ok(tube, 'test tube created')
-test:is(tube.name, 'test', 'tube.name')
+test:is(tube.name, 'lim3_tube', 'tube.name')
 test:is(tube.type, 'limfifottl', 'tube.type')
 
 test:test('Put timeout is reached', function(test)
@@ -44,6 +45,14 @@ test:test('Put after freeing up space', function(test)
     while put_fiber:status() ~= 'dead' do
         fiber.yield()
     end
+end)
+
+test:test('Unlimited tube put', function(test)
+    test:plan(3)
+
+    test:is(unlim_tube:take(0), nil, 'tube is empty')
+    test:ok(unlim_tube:put{1}, 'task 1 was put')
+    test:ok(unlim_tube:put{2}, 'task 2 was put')
 end)
 
 tnt.finish()
