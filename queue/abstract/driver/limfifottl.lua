76ebb7b8304ec91669ec471bf22053407c95cd2a
Andrei Nepsha <nepsha.a.a@ya.ru>
2019-07-12 10:47:22 +0300

    Simplify limfifottl timeout calculation

--- queue/abstract/driver/limfifottl.lua
+++ queue/abstract/driver/limfifottl.lua
@@ -20,21 +20,17 @@ function tube.new(space, on_task_change, opts)
     -- put task in space
     local put = function (self, data, opts)
         local timeout = opts.timeout or 0
+        local started = tonumber(fiber.time())
 
         while true do
             local tube_size = self.space:len()
             if tube_size < state.capacity or state.capacity == 0 then
                 return state.parent.put(self, data, opts)
             else
-                if timeout == 0 then
+                if tonumber(fiber.time()) - started > timeout then
                     return nil
                 end
-
-                local started = fiber.time()
                 fiber.sleep(.01)
-                local elapsed = fiber.time() - started
-
-                timeout = timeout > elapsed and timeout - elapsed or 0
             end
         end
     end
