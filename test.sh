e29dc72cb3b83a6e7f53cc6008de6c83cf5d3d59
Leonid Vasiliev <lvasiliev@tarantool.org>
2019-12-06 14:45:56 +0300

    travis-ci: handle packagecloud's HTTP redirects
    Added curl options:
    
    * -L (--location) to follow Location header value from 3XX responses,
      because packagecloud.io redirects a GPG key request to a CDN.
    * -s (--silent) to don't show a progress meter.
    * -f (--fail) to fail the command at non 2XX HTTP responses. It'll fail
      `curl | <...>` command pipeline, since `set -o pipefail` is now
      enabled.
    
    Aside of that, fail test.sh script (and so a CI job) if something inside
    the script fails (`set -e`) and catch mistakes from using uninitialized
    bash variables (`set -u`) inside the script.

--- test.sh
+++ test.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+set -euo pipefail
+
 VERSION=${VERSION:-2_2}
 
-curl https://packagecloud.io/tarantool/${VERSION}/gpgkey | sudo apt-key add -
+curl -sfL https://packagecloud.io/tarantool/${VERSION}/gpgkey | sudo apt-key add -
 release=`lsb_release -c -s`
 
 sudo apt-get install -y apt-transport-https
 
 sudo tee /etc/apt/sources.list.d/tarantool_${VERSION}.list <<- EOF
-deb https://packagecloud.io/tarantool/${VERSION}/ubuntu/ $release main
-deb-src https://packagecloud.io/tarantool/${VERSION}/ubuntu/ $release main
+deb https://packagecloud.io/tarantool/${VERSION}/ubuntu/ ${release} main
+deb-src https://packagecloud.io/tarantool/${VERSION}/ubuntu/ ${release} main
 EOF
 
 sudo apt-get update > /dev/null
